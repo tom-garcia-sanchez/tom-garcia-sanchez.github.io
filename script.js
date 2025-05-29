9f168dec3a15c9f7a4d860cf1581962d97737072
ikram-md <jazzthedreameryeah@gmail.com>
2025-05-30 00:49:56 +0200

    Added the navbar

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("menu-toggle").addEventListener("click", function () {
+    const navLinks = document.getElementById("nav-links");
+    navLinks.classList.toggle("open");
+  });
+  
